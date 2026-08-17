abde01f1b5090ae15a4f16f7c35c5cf1020e6646
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-08-17 18:10:04 -0500

    Update testes.py

--- testes.py
+++ testes.py
@@ -9,3 +9,11 @@ print(f"Tabelas criadas: {bancoTeste.tables}")
 
 bancoTeste.create_table("users")
 print(f"Tabelas criadas: {bancoTeste.tables}")
+
+print("========TESTES DE CRIAÇÃO DE COLUNAS========")
+bancoTeste.tables[0].add_column("ID","INTEGER")
+print(f"Colunas criadas: {bancoTeste.tables[0].columns}")
+bancoTeste.tables[0].add_column("User","TEXT")
+print(f"Colunas criadas: {bancoTeste.tables[0].columns}")
+bancoTeste.tables[0].add_column("ID","INTEGER")
+print(f"Colunas criadas: {bancoTeste.tables[0].columns}")
