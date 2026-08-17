09c9abef57afa1cd90df448f11b4b6cb6215966a
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-08-17 17:53:17 -0500

    Update table.py

--- table.py
+++ table.py
@@ -1,4 +1,14 @@
+from column import Column
 class Table:
     def __init__(self,name):
         self.name = name
         self.columns = []
+
+    def add_column(self, name, type_data):
+        column_exists = False
+        for column in self.columns:
+            if column.name == name:
+                column_exists = True
+                break
+        if column_exists == False:
+            self.columns.append(Column(name, type_data))
