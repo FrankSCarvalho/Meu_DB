932d3e3f2c78f1d6d2de3ee84782d91ebaa7dfd6
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-08-17 16:26:32 -0500

    -

--- database.py
+++ database.py
@@ -1,5 +1,10 @@
+from table import Table
 
 class Database:
     def __init__(self, name):
         self.name = name
         self.tables = []
+
+    def create_table(self, name):
+        self.table = Table(name)
+        self.tables.append(self.table)
--- table.py
+++ table.py
@@ -1,3 +1,3 @@
 class Table:
     def __init__(self,name):
-        self.name = name
+        self.name = name    
